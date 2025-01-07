e50c5d3d4b4f6e8d632be889dbe89ac557c6c62c
edaagunes <edagunes00@hotmail.com>
2025-01-07 16:00:03 +0300

    Add project files.

---
+++ BootcampBookProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BootcampBookProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
